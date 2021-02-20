1952ec170bd49bf4aa76f33c7a4075d38e1fdb3d
Bailey <bailey.elise.dennis@gmail.com>
2021-02-20 13:22:58 -0600

    notes from class

---
+++ LoafAndStranger/Controllers/Loaves.cs
@@ -0,0 +1,31 @@
+﻿using LoafAndStranger.Models;
+using Microsoft.AspNetCore.Mvc;
+using LoafAndStranger.DataAccess;
+
+namespace LoafAndStranger.Controllers
+{
+    [Route("api/Loaves")]
+    [ApiController]
+    public class LoavesController : ControllerBase
+    {
+        LoafRepository _repo;
+        public LoavesController()
+        {
+            _repo = new LoafRepository();
+        }
+
+        //if its gonna be exposed to the internet, must be PUBLIC
+        [HttpGet]
+        public IActionResult GetAllLoaves()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult AddALoaf (Loaf loaf)
+        {
+            _repo.Add(loaf);
+            return Created($"api/Loaves/{loaf.Id}", loaf);
+        }
+    }
+}
---
+++ LoafAndStranger/Models/Loaf.cs
@@ -0,0 +1,20 @@
+﻿namespace LoafAndStranger.Models
+{
+    //Models are for storing pieces of information, not for having behavior.
+    public class Loaf
+    {
+        public LoafSize Size { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
+        public int WeightInOunces { get; set; }
+        public bool Sliced { get; set; }
+    }
+
+    //as long as it isn't used anywhere else, this can be in the same file
+    public enum LoafSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
