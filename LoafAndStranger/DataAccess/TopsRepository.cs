e6852aa9efdf07b2c508c17defc96a1ad0bd2bc7
Bailey <bailey.elise.dennis@gmail.com>
2021-03-16 20:57:06 -0500

    added tops controller

---
+++ LoafAndStranger/DataAccess/TopsRepository.cs
@@ -0,0 +1,25 @@
+﻿using LoafAndStranger.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace LoafAndStranger.DataAccess
+{
+    public class TopsRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=LoafAndStranger;Trusted_Connection=True;";
+        public IEnumerable<Top> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = "SELECT * FROM Tops";
+
+            var results = db.Query<Top>(sql);
+
+            return results;
+        }
+    }
+}
